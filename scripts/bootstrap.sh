ac817b5d28e8423ba228f4ccc0e6e78941abb79f
SevenCats <sevencats@tripsit.me>
2021-01-24 01:26:52 -0500

    clean script and improvements to bootstrapping

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,12 +1,22 @@
 
 for project_id in "${ALL_PROJECTS[@]}"; do
 	project_path="${REPOS_PATH}/${project_id}"
-	# Clone git repositories that don't exist
+
+	# Only bootstrap projects that don't already exist in the repos directory
 	if [[ ! -d "${project_path}" ]]; then
+
+		# Clone git repo
 		git clone "git@github.com:TripSit/${project_id}.git" "${project_path}"
+
 		# If npm package npm install
 		if [[ -f "${project_path}/package.json" ]]; then
 			npm --prefix "${project_path}" install
+			npm --prefix "${project_path}" run setup 2> /dev/null || true
+		fi
+
+		# Copy over example .env file
+		if [[ -f "${project_path}/.env.example" ]]; then
+			cp "${project_path}/.env.example" "${project_path}/.env"
 		fi
 	fi
 done
--- scripts/clean.sh
+++ scripts/clean.sh
@@ -6,3 +6,18 @@ echo "Removing repositories..."
 for project_id in ${ALL_PROJECTS[@]}; do
 	rm -rf "${REPOS_PATH}/${project_id}"
 done
+
+# Remove Docker containers and volumes
+containers="$(docker ps -qaf name=tripsit)"
+if [[ ! -z "${containers}" ]]; then
+	running_containers="$(docker ps -qf name=tripsit)"
+	if [[ ! -z "${running_containers}" ]]; then
+		docker stop "${running_containers}"
+	fi
+	docker rm "${containers}"
+fi
+
+container_volumes="$(docker volume ls -qf name=tripsit)"
+if [[ ! -z "${container_volumes}" ]]; then
+	docker volume rm "${container_volumes}"
+fi
