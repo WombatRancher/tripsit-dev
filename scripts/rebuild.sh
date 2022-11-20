b547ce8206736511f553d2344772a02aaf673193
Moonbear <1836049+LunaUrsa@users.noreply.github.com>
2022-11-19 19:42:47 -0600

    Update dependances

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -1,16 +1,9 @@
 
-echo "Rebuilding containers"
-for project_id in "${DOCKER_COMPOSE_PROJECT_IDS[@]}"; do
-	docker compose \
-		-f "${REPOS_PATH}/${project_id}/docker-compose.yml" \
-		build
-done
-
 echo "Updating containers"
 for project_id in "${DOCKER_COMPOSE_PROJECT_IDS[@]}"; do
 	docker compose \
 		-f "${REPOS_PATH}/${project_id}/docker-compose.yml" \
-		up -d
+		up -d --build
 done
 
 echo "Done starting containers"
