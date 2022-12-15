5b83e7b7b4ccf9b66ed19fade9cfc483af5a0467
LunaUrsa <1836049+LunaUrsa@users.noreply.github.com>
2022-12-15 16:03:19 +0000

    Minor udpates to flow

--- scripts/up.sh
+++ scripts/up.sh
@@ -13,16 +13,16 @@ for project_id in "${DOCKER_COMPOSE_PROJECT_IDS[@]}"; do
 		up -d --build
 		# --env-file ./.env up -d --build
 	# Run only on the http-api project
-	if [ ${project_id} = http-api ]; then
-	  sleep 10
-		project_path="${REPOS_PATH}/${project_id}"
-		echo "Refreshing migrations on ${project_id}"
-		npm --prefix "${project_path}" run reset-db
-		# echo "Running migration on ${project_id}"
-		# npm --prefix "${project_path}" knex migrate:latest
-		# echo "Running seed on ${project_id}"
-		# npm --prefix "${project_path}" knex seed:run
-	fi
+	# if [ ${project_id} = http-api ]; then
+	#   sleep 10
+	# 	project_path="${REPOS_PATH}/${project_id}"
+	# 	echo "Refreshing migrations on ${project_id}"
+	# 	npm --prefix "${project_path}" run reset-db
+	# 	# echo "Running migration on ${project_id}"
+	# 	# npm --prefix "${project_path}" knex migrate:latest
+	# 	# echo "Running seed on ${project_id}"
+	# 	# npm --prefix "${project_path}" knex seed:run
+	# fi
 done
 
 echo "Done starting containers"
