733178adcea98a8934ab9cfa51048310ed0f317e
Moonbear <1836049+LunaUrsa@users.noreply.github.com>
2023-04-01 12:37:58 -0500

    Reorder scripts to move the 'delete' scripts to the bottom

--- scripts/up.sh
+++ scripts/up.sh
@@ -12,17 +12,17 @@ for project_id in "${DOCKER_COMPOSE_PROJECT_IDS[@]}"; do
 		-f "${REPOS_PATH}/${project_id}/docker-compose.yml" \
 		up -d --build
 		# --env-file ./.env up -d --build
-	# Run only on the http-api project
-	# if [ ${project_id} = http-api ]; then
-	#   sleep 10
-	# 	project_path="${REPOS_PATH}/${project_id}"
-	# 	echo "Refreshing migrations on ${project_id}"
-	# 	npm --prefix "${project_path}" run reset-db
-	# 	# echo "Running migration on ${project_id}"
-	# 	# npm --prefix "${project_path}" knex migrate:latest
-	# 	# echo "Running seed on ${project_id}"
-	# 	# npm --prefix "${project_path}" knex seed:run
-	# fi
+		# Run only on the http-api project
+		# if [ ${project_id} = http-api ]; then
+		#   sleep 10
+		# 	project_path="${REPOS_PATH}/${project_id}"
+		# 	echo "Refreshing migrations on ${project_id}"
+		# 	npm --prefix "${project_path}" run reset-db
+		# 	# echo "Running migration on ${project_id}"
+		# 	# npm --prefix "${project_path}" knex migrate:latest
+		# 	# echo "Running seed on ${project_id}"
+		# 	# npm --prefix "${project_path}" knex seed:run
+		# fi
 done
 
 echo "Done starting containers"
